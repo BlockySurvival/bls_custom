e464895bcb552b04e1c2b67ea2f13717b1c2b246
y <y@y>
2019-05-20 05:30:23 +0100

    elevator recipe fixes

--- init.lua
+++ init.lua
@@ -22,3 +22,25 @@ if minetest.get_modpath("gravelsieve") then
     minetest.register_alias_force("gravelsieve:compressed_gravel", "default:cobble")
     minetest.clear_craft({output="gravelsieve:compressed_gravel"})
 end
+
+if minetest.get_modpath("travelnet") and minetest.get_modpath("titanium") then
+    minetest.clear_craft({output="travelnet:elevator"})
+    minetest.register_craft({
+        output = "travelnet:elevator",
+        recipe = {
+            {"default:steel_ingot", "default:glass",  "default:steel_ingot", },
+            {"default:steel_ingot", "titanium:block", "default:steel_ingot", },
+            {"default:steel_ingot", "default:glass",  "default:steel_ingot", }
+        },
+    })
+
+    minetest.clear_craft({output="travelnet:travelnet"})
+    minetest.register_craft({
+        output = "travelnet:travelnet",
+        recipe = {
+            {"default:glass", "titanium:block", "default:glass", },
+            {"default:mese",  "default:mese",   "default:mese", },
+            {"default:glass", "titanium:block", "default:glass", }
+        },
+    })
+end
