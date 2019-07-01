0576b71a64f90601a81d71b5f628958fc3158254
y <y@y>
2019-07-01 03:47:29 +0100

    name for fake atm

--- new_nodes.lua
+++ new_nodes.lua
@@ -1,7 +1,7 @@
 local get_modpath = minetest.get_modpath
 
 if get_modpath('lurkcoin') and get_modpath('default') and get_modpath('dye') then
-    minetest.register_node('lurkcoin:dummy_atm', {
+    minetest.register_node(':lurkcoin:dummy_atm', {
         description = 'Dummy ATM',
         groups = {cracky = 1},
         tiles = {'lurkcoin_atm_side.png', 'lurkcoin_atm_side.png',
