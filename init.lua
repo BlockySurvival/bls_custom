4c59c7e2a9898149f06bc910912c7daf7b5ce00d
luk3yx <luk3yx@users.noreply.github.com>
2019-07-04 17:29:54 +1200

    Add /crash

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ if minetest.get_modpath('doors') and doors.door_toggle then
 end
 
 dofile(minetest.get_modpath('bls_overrides') .. '/aliases.lua')
+dofile(minetest.get_modpath('bls_overrides') .. '/antitroll.lua')
 dofile(minetest.get_modpath('bls_overrides') .. '/crafting.lua')
 dofile(minetest.get_modpath('bls_overrides') .. '/item_overrides.lua')
 dofile(minetest.get_modpath('bls_overrides') .. '/hunger_overrides.lua')
