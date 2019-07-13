45b5b1a54e06d963ffd9eeb48cdf6c7929553b46
y <y@y>
2019-07-13 23:25:33 +0100

    merge nogrief into this mod

--- monkey_patching.lua
+++ monkey_patching.lua
@@ -51,3 +51,23 @@ if global_exists('tnt') then
         end
     end
 end
+
+minetest.register_privilege("lava", {description = "Allows a player to place lava anywhere", give_to_singleplayer = false})
+
+local oldplace = minetest.item_place
+minetest.item_place = function (itemstack, placer, pointed_thing, param2)
+	if not pointed_thing.above then
+		return
+	end
+	local player_name = placer:get_player_name()
+	if pointed_thing.above.y > -5 and not minetest.check_player_privs(player_name, {lava = true}) then
+		if itemstack:get_name() == "default:lava_source" then
+			minetest.chat_send_player(player_name, "You cannot place lava over -5m", true)
+			return itemstack
+		elseif itemstack:get_name() == "terumet:mach_lavam" then
+			minetest.chat_send_player(player_name, "You cannot place a Lava Melter above -5m", true)
+			return itemstack
+		end
+	end
+	return oldplace(itemstack, placer, pointed_thing, param2)
+end
