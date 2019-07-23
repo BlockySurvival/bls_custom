09948525ba56645ee2f22b28d1ef06c3f7aba74f
y <y@y>
2019-07-23 13:15:26 +0000

    rename a function, add some notes

--- fix_privs.lua
+++ fix_privs.lua
@@ -1,7 +1,7 @@
 local storage = bls.mod_storage
 local YES_VALUE = 'y'
 
-local function fix_tp_priv(name)
+local function fix_priv_1(name)
     local key = ('%s_fix_priv_1'):format(name)
     if storage:get_string(key) ~= YES_VALUE then
         local privs = minetest.get_player_privs(name) or {}
@@ -14,7 +14,11 @@ local function fix_tp_priv(name)
     end
 end
 
-minetest.register_on_joinplayer(bls.util.safe(function(player)
+-- NOTE: if privileges change AGAIN then you have to create a new key and function like the above.
+-- NOTE: you can reuse the same on_player_join though.
+
+minetest.register_on_joinplayer(function(player)
     local name = player:get_player_name()
-    minetest.after(0, fix_tp_priv, name)
-end))
+    minetest.after(0, fix_priv_1, name)
+end)
+
