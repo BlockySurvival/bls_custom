b4e4b374711c6580cbdd53b19ff5073ed94ccf37
Niwla23 <46248939+Niwla23@users.noreply.github.com>
2019-07-29 14:08:45 +0200

    Create reformer.lua

---
+++ reformer.lua
@@ -0,0 +1,33 @@
+terumet.register_repairable_item("titanium:pick", 300)
+terumet.register_repairable_item("titanium:axe", 300)
+terumet.register_repairable_item("titanium:shovel", 300)
+terumet.register_repairable_item("titanium:sword", 300)
+
+terumet.register_repairable_item("hangglider:hangglider", 500)
+
+terumet.register_repairable_item("farming:scyte_mythril", 30)
+
+terumet.register_repairable_item("goldtools:goldpick", 30)
+terumet.register_repairable_item("goldtools:goldaxe", 30)
+terumet.register_repairable_item("goldtools:goldshovel", 30)
+terumet.register_repairable_item("goldtools:goldsword", 30)
+
+terumet.register_repairable_item("mobs:net", 10)
+terumet.register_repairable_item("mobs:lasso", 100)
+terumet.register_repairable_item("mobs:shears", 10)
+
+terumet.register_repairable_item("lavastuff:pick", 250)
+terumet.register_repairable_item("lavastuff:axe", 250)
+terumet.register_repairable_item("lavastuff:shovel", 250)
+terumet.register_repairable_item("lavastuff:sword", 250)
+terumet.register_repairable_item("moreores:axe_mithril", 30)
+terumet.register_repairable_item("moreores:hoe_mithril", 30)
+terumet.register_repairable_item("moreores:pick_mithril", 30)
+terumet.register_repairable_item("moreores:sword_mithril", 30)
+terumet.register_repairable_item("moreores:shovel_mithril", 30)
+
+terumet.register_repairable_item("moreores:axe_silver", 30)
+terumet.register_repairable_item("moreores:hoe_silver", 30)
+terumet.register_repairable_item("moreores:pick_silver", 30)
+terumet.register_repairable_item("moreores:sword_silver", 30)
+terumet.register_repairable_item("moreores:shovel_silver", 30)
