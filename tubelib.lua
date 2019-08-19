38973432470a6070338ca3083e74f1758bffeb35
flux <25628292+fluxionary@users.noreply.github.com>
2019-08-19 12:51:27 +0000

    fix

--- tubelib.lua
+++ tubelib.lua
@@ -2,7 +2,7 @@ if minetest.global_exists('tubelib_addons1') then
     local function register_tree(sapling, trunk, leaves)
         tubelib_addons1.register_tree_node(trunk, trunk, sapling)
         tubelib_addons1.register_farming_node(leaves)
-        tubelib.add_grinder_recipe({input=trunk_name, output=leaves_name .. ' 8'})
+        tubelib.add_grinder_recipe({input=trunk, output=leaves .. ' 8'})
     end
 
     register_tree('redtrees:sapling', 'redtrees:rtree', 'redtrees:rleaves')
