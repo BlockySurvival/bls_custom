0cdc3c8ef8ab1212c4245244093a6e317e63bd60
flux <25628292+fluxionary@users.noreply.github.com>
2019-08-31 20:50:10 +0000

    remove uranium ore generation (forked technic)

--- oregen.lua
+++ oregen.lua
@@ -2,29 +2,6 @@ minetest.override_item("default:steel_ingot", {
 	inventory_image = "default_steel_ingot.png",
 })
 
-local uranium_params = {
-	offset = 0,
-	scale = 1,
-	spread = {x = 100, y = 100, z = 100},
-	seed = 420,
-	octaves = 3,
-	persist = 0.7
-}
-local uranium_threshold = 0.55
-
-minetest.register_ore({
-	ore_type = "scatter",
-	ore = "technic:mineral_uranium",
-	wherein = "default:stone",
-	clust_scarcity = 6*6*6,
-	clust_num_ores = 4,
-	clust_size = 3,
-	y_min = -25000,
-	y_max = -300,
-	noise_params = uranium_params,
-	noise_threshold = uranium_threshold,
-})
-
 minetest.register_ore({
     ore_type = "sheet",
     ore = "default:clay",
