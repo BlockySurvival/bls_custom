e00c8a82b4b8e080424717e093c29b682e105fb0
flux <25628292+fluxionary@users.noreply.github.com>
2019-09-14 04:47:40 +0000

    revokable caps priv

---
+++ privs.lua
@@ -0,0 +1,25 @@
+minetest.register_privilege("caps", "Allows a player to use capital letters in chat")
+
+minetest.register_on_chat_message(function(name, message)
+    bls.log("action", "caps: register_on_chat_message")
+    if not minetest.check_player_privs(name, {caps=true}) then
+        local m
+        if minetest.format_chat_message then
+            m = minetest.format_chat_message(name, message:lower())
+        else
+            m = ("<%s> %s"):format(name, message:lower())
+        end
+        minetest.chat_send_all(m)
+        return true
+    end
+end)
+--
+--local old_format_chat_message = minetest.format_chat_message
+--
+--function minetest.format_chat_message(name, message)
+--    bls.log("action", "caps: format_chat_message")
+--    if not minetest.check_player_privs(name, {caps=true}) then
+--        message = message:lower()
+--    end
+--    return old_format_chat_message(name, message) .. " TEST"
+--end
--- update_initial_privs.lua
+++ update_initial_privs.lua
@@ -7,9 +7,21 @@ local function fix_priv_1(name)
     local key = ('%s_fix_priv_1'):format(name)
     if storage:get_string(key) ~= YES_VALUE then
         local privs = minetest.get_player_privs(name) or {}
-        if privs.interact and privs.shout and privs.home then
+        if privs.interact and privs.shout then
             privs.tp = true
             privs.instruments = true
+            minetest.set_player_privs(name, privs)
+        end
+        storage:set_string(key, YES_VALUE)
+    end
+end
+
+local function fix_priv_2(name)
+    local key = ('%s_fix_priv_2'):format(name)
+    if storage:get_string(key) ~= YES_VALUE then
+        local privs = minetest.get_player_privs(name) or {}
+        if privs.shout then
+            privs.caps = true
             minetest.set_player_privs(name, privs)
         end
         storage:set_string(key, YES_VALUE)
@@ -22,5 +34,6 @@ end
 minetest.register_on_joinplayer(function(player)
     local name = player:get_player_name()
     minetest.after(0, fix_priv_1, name)
+    minetest.after(0, fix_priv_2, name)
 end)
 
