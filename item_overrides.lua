2999bea5f765c0f107b19793299a0c7794556cf1
flux <25628292+fluxionary@users.noreply.github.com>
2019-10-28 12:10:49 +0000

    add some groups

--- item_overrides.lua
+++ item_overrides.lua
@@ -1,19 +1,42 @@
 -- PLEASE KEEP MOD SECTIONS IN ALPHABETICAL ORDER
 -- ORGANIZE LOGIC BY THE OVERRIDDEN ITEM
 
+local function add_groups(itemstring, ...)
+    local groups = table.copy(minetest.registered_items[itemstring].groups)
+    for group in ipairs(...) do
+        groups[group] = 1
+    end
+    minetest.override_item(itemstring, {groups=groups})
+end
+
 if minetest.get_modpath('caverealms') then
     -- make thin ice slippery
-    minetest.override_item('caverealms:thin_ice', {
-        groups={cracky=3, slippery=5},
-    })
-    minetest.override_item('caverealms:hanging_thin_ice', {
-        groups={cracky=3, slippery=5},
-    })
+    add_groups("caverealms:thin_ice", "cracky", "slippery")
+    add_groups("caverealms:hanging_thin_ice", "cracky", "slippery")
+end
+
+if minetest.get_modpath("cucina_vegana") then
+end
+
+if minetest.get_modpath("extra") then
+    add_groups("extra:cottonseed_oil", "food_oil", "food_vegan")
+    add_groups("extra:fish_sticks", "food_fish")
+end
+if minetest.get_modpath("mobs_animal") then
+    add_groups("mobs:rat_cooked", "food_meat")
+end
+if minetest.get_modpath("mobs_fish") then
+    add_groups("mobs_fish:clownfish", "food_fish")
+    add_groups("mobs_fish:tropical", "food_fish")
+end
+if minetest.get_modpath("mobs_jellyfish") then
+    add_groups("mobs_jellyfish:jellyfish", "food_fish")
 end
 
 if minetest.global_exists('maptools') then
     -- Temporarily disable pushers because rats in the trampoline
     for pusher_num = 1, 10 do
+        add_groups("extra:cottonseed_oil", "food_oil", "food_vegan")
         minetest.override_item('maptools:pusher_' .. pusher_num, {
             groups = {
                 unbreakable = 1,
