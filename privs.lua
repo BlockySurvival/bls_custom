6348310d205dd86488727956a00ff3acb808cac4
flux <25628292+fluxionary@users.noreply.github.com>
2019-10-28 12:11:40 +0000

    privs, microblocks

--- privs.lua
+++ privs.lua
@@ -1,7 +1,6 @@
 minetest.register_privilege("caps", "Allows a player to use capital letters in chat")
 
 minetest.register_on_chat_message(function(name, message)
-    bls.log("action", "caps: register_on_chat_message")
     if not minetest.check_player_privs(name, {caps=true}) then
         local m
         if minetest.format_chat_message then
