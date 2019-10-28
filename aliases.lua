959efa1042121be826d74c3243b47a95e487e52e
flux <25628292+fluxionary@users.noreply.github.com>
2019-10-28 12:10:11 +0000

    remove broken microblocks aliases

--- aliases.lua
+++ aliases.lua
@@ -1,5 +1,4 @@
--- PLEASE KEEP MOD SECTIONS IN ALPHABETICAL ORDER
--- ORGANIZE LOGIC BY THE SOURCE ITEM
+
 
 -- deal with items that have now been moved into this mod
 minetest.register_alias_force("core:honey_bottle", "bls:honey_bottle")
@@ -34,54 +33,8 @@ minetest.register_alias_force("bls_overrides:door_steel_protected_b", "bls:door_
 minetest.register_alias_force("bls_admin_flair:shield_bls", "bls:shield_bls")
 minetest.register_alias_force("bls_admin_flair:shield_staff", "bls:shield_staff")
 
----------------
-
-local function fix(mod, kind, prefix)
-    if not prefix then prefix = "" end
-    minetest.register_alias_force("stairs:slab_" .. prefix .. kind, mod .. ":slab_" .. prefix .. kind)
-    minetest.register_alias_force("stairs:stair_" .. prefix .. kind, mod .. ":stair_" .. prefix .. kind)
-    minetest.register_alias_force("stairs:stair_inner_" .. prefix .. kind, mod .. ":stair_" .. prefix .. kind .. "_inner")
-    minetest.register_alias_force("stairs:stair_outer_" .. prefix .. kind, mod .. ":stair_" .. prefix .. kind .. "_outer")
-end
-
-if minetest.get_modpath("bakedclay") then
-    local COLORS = {
-        'black', 'blue', 'brown', 'cyan', 'dark_green', 'dark_grey', 'green', 'grey',
-        'magenta', 'orange', 'pink', 'red', 'violet', 'white', 'yellow', "natural"
-    }
-
-    for _, color in ipairs(COLORS) do
-        fix("bakedclay", color, "baked_clay_")
-    end
-end
-
-for kind in ipairs({"block", "pillar", "chiseled"}) do
-    fix("quartz", kind)
-end
-
-for color in ipairs({"green", "yellow", "red", "steel", "white"}) do
-    fix("bridger", color, "block_")
-end
-
-for kind in ipairs({"rack", "brick", "obsidian_enchanted"}) do
-    fix("nether", kind)
-end
-
-for kind in ipairs({"hardwood", "adobe", "Marble", "Tar", "fakegrass", }) do
-    fix("building_blocks", kind)
-end
-
-local COLORS = {
-    'black', 'blue', 'brown', 'cyan', 'dark_green', 'dark_grey', 'green', 'grey',
-    'magenta', 'orange', 'pink', 'red', 'violet', 'white', 'yellow'
-}
-
-for _, color in ipairs(COLORS) do
-    fix("cblocks", color, "glass_")
-    fix("cblocks", color, "stonebrick_")
-    fix("cblocks", color, "wood_")
-end
-
+-- PLEASE KEEP MOD SECTIONS IN ALPHABETICAL ORDER
+-- ORGANIZE LOGIC BY THE SOURCE ITEM
 
 if minetest.get_modpath('farming') then
     minetest.register_alias_force('default:blueberries', 'farming:blueberries')
@@ -144,4 +97,6 @@ if minetest.get_modpath('xdecor') and minetest.get_modpath('farming')then
     minetest.register_alias_force("xdecor:bowl", "farming:bowl")
 end
 
-minetest.register_alias_force("trunks:twig_1", "default:stick")
+if minetest.get_modpath("trunks") then
+    minetest.register_alias_force("trunks:twig_1", "default:stick")
+end
