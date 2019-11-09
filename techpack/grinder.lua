338017a1f0384ee39d2867896beb0a63f033fc57
flux <25628292+fluxionary@users.noreply.github.com>
2019-11-09 20:00:50 +0000

    cool trees / techpack integration

--- techpack/grinder.lua
+++ techpack/grinder.lua
@@ -1,6 +1,41 @@
 if not minetest.get_modpath("tubelib_addons1") then return end
+local gr = tubelib.add_grinder_recipe
 
 if minetest.get_modpath("bonemeal") then
-    tubelib.add_grinder_recipe({input="bonemeal:bone", output="bonemeal:bonemeal 4"})
-    tubelib.add_grinder_recipe({input="default:coral_skeleton", output="bonemeal:bonemeal 4"})
+    gr({input="bonemeal:bone", output="bonemeal:bonemeal 4"})
+    gr({input="default:coral_skeleton", output="bonemeal:bonemeal 4"})
+end
+
+if minetest.get_modpath("bamboo") then
+    gr({input="bamboo:trunk", output="bamboo:leaves 8"})
+end
+if minetest.get_modpath("birch") then
+    gr({input="birch:trunk", output="birch:leaves 8"})
+end
+if minetest.get_modpath("cherrytree") then
+    gr({input="cherrytree:trunk", output="cherrytree:leaves 8"})
+end
+if minetest.get_modpath("chestnuttree") then
+    gr({input="chestnuttree:trunk", output="chestnuttree:leaves 8"})
+end
+if minetest.get_modpath("clementinetree") then
+    gr({input="clementinetree:trunk", output="clementinetree:leaves 8"})
+end
+if minetest.get_modpath("ebony") then
+    gr({input="ebony:trunk", output="ebony:leaves 8"})
+end
+if minetest.get_modpath("jacaranda") then
+    gr({input="jacaranda:trunk", output="jacaranda:leaves 8"})
+end
+if minetest.get_modpath("larch") then
+    gr({input="larch:trunk", output="larch:leaves 8"})
+end
+if minetest.get_modpath("lemontree") then
+    gr({input="lemontree:trunk", output="lemontree:leaves 8"})
+end
+if minetest.get_modpath("mahogany") then
+    gr({input="mahogany:trunk", output="mahogany:leaves 8"})
+end
+if minetest.get_modpath("palm") then
+    gr({input="palm:trunk", output="palm:leaves 8"})
 end
--- techpack/harvester.lua
+++ techpack/harvester.lua
@@ -1,5 +1,7 @@
 if not minetest.get_modpath("tubelib_addons1") then return end
-local dn = tubelib_addons1.register_default_farming_node
+local tn = tubelib_addons1.register_tree_node  -- (name, drop, plant)
+local dn = tubelib_addons1.register_default_farming_node  -- (name, drop, plant)
+local fn = tubelib_addons1.register_farming_node  -- (name, drop, plant)
 
 if minetest.get_modpath("cucina_vegana") then
     dn("cucina_vegana:parsley_5", "cucina_vegana:parsley", "cucina_vegana:parsley_1")
@@ -14,3 +16,63 @@ if minetest.get_modpath("cucina_vegana") then
     dn("cucina_vegana:rice_6", "cucina_vegana:rice", "cucina_vegana:rice_1")
     dn("cucina_vegana:sunflower_5", "cucina_vegana:sunflower", "cucina_vegana:sunflower_1")
 end
+
+if minetest.get_modpath("bamboo") then
+    tn("bamboo:trunk", "bamboo:trunk", "bamboo:sprout")
+    fn("bamboo:leaves")
+end
+if minetest.get_modpath("birch") then
+    tn("birch:trunk", "birch:trunk", "birch:sapling")
+    fn("birch:leaves")
+end
+if minetest.get_modpath("cherrytree") then
+    tn("cherrytree:trunk", "cherrytree:trunk", "cherrytree:sapling")
+    fn("cherrytree:leaves")
+    fn("cherrytree:blossom_leaves")
+    fn("cherrytree:cherries")
+end
+if minetest.get_modpath("chestnuttree") then
+    tn("chestnuttree:trunk", "chestnuttree:trunk", "chestnuttree:sapling")
+    fn("chestnuttree:leaves")
+    fn("chestnuttree:fruit")
+end
+if minetest.get_modpath("clementinetree") then
+    tn("clementinetree:trunk", "clementinetree:trunk", "clementinetree:sapling")
+    fn("clementinetree:leaves")
+    fn("clementinetree:clementine")
+end
+if minetest.get_modpath("ebony") then
+    tn("ebony:trunk", "ebony:trunk", "ebony:sapling")
+    fn("ebony:leaves")
+    fn("ebony:creeper")
+    fn("ebony:creeper_leaves")
+    fn("ebony:liana")
+end
+if minetest.get_modpath("jacaranda") then
+    tn("jacaranda:trunk", "jacaranda:trunk", "jacaranda:sapling")
+    fn("jacaranda:leaves")
+    fn("jacaranda:blossom_leaves")
+end
+if minetest.get_modpath("larch") then
+    tn("larch:trunk", "larch:trunk", "larch:sapling")
+    fn("larch:leaves")
+    fn("larch:moss")
+end
+if minetest.get_modpath("lemontree") then
+    tn("lemontree:trunk", "lemontree:trunk", "lemontree:sapling")
+    fn("lemontree:leaves")
+    fn("lemontree:lemon")
+end
+if minetest.get_modpath("mahogany") then
+    tn("mahogany:trunk", "mahogany:trunk", "mahogany:sapling")
+    fn("mahogany:leaves")
+    fn("mahogany:creeper")
+    fn("mahogany:flower_creeper")
+    fn("mahogany:hanging_creeper")
+end
+if minetest.get_modpath("palm") then
+    tn("palm:trunk", "palm:trunk", "palm:sapling")
+    fn("palm:leaves")
+    fn("palm:coconut")
+end
+
