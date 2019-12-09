3a2fec3a9fe758d6bb99add5ff4008b025de74d5
flux <25628292+fluxionary@users.noreply.github.com>
2019-12-09 23:56:15 +0000

    crops give multiple items, not single items.

--- techpack/harvester.lua
+++ techpack/harvester.lua
@@ -1,20 +1,19 @@
 if not minetest.get_modpath("tubelib_addons1") then return end
 local tn = tubelib_addons1.register_tree_node  -- (name, drop, plant)
-local dn = tubelib_addons1.register_default_farming_node  -- (name, drop, plant)
 local fn = tubelib_addons1.register_farming_node  -- (name, drop, plant)
 
 if minetest.get_modpath("cucina_vegana") then
-    dn("cucina_vegana:parsley_5", "cucina_vegana:parsley", "cucina_vegana:parsley_1")
-    dn("cucina_vegana:rosemary_6", "cucina_vegana:rosemary", "cucina_vegana:rosemary_1")
-    dn("cucina_vegana:chives_5", "cucina_vegana:chives", "cucina_vegana:chives_1")
-    dn("cucina_vegana:flax_6", "cucina_vegana:flax_raw", "cucina_vegana:flax_1")
-    dn("cucina_vegana:kohlrabi_6", "cucina_vegana:kohlrabi", "cucina_vegana:kohlrabi_1")
-    dn("cucina_vegana:asparagus_6", "cucina_vegana:asparagus", "cucina_vegana:asparagus_1")
-    dn("cucina_vegana:lettuce_5", "cucina_vegana:lettuce", "cucina_vegana:lettuce_1")
-    dn("cucina_vegana:soy_8", "cucina_vegana:soy", "cucina_vegana:soy_1")
-    dn("cucina_vegana:peanut_7", "cucina_vegana:peanut", "cucina_vegana:peanut_1")
-    dn("cucina_vegana:rice_6", "cucina_vegana:rice", "cucina_vegana:rice_1")
-    dn("cucina_vegana:sunflower_5", "cucina_vegana:sunflower", "cucina_vegana:sunflower_1")
+    fn("cucina_vegana:parsley_5", "cucina_vegana:parsley 4", "cucina_vegana:parsley_1")
+    fn("cucina_vegana:rosemary_6", "cucina_vegana:rosemary 4", "cucina_vegana:rosemary_1")
+    fn("cucina_vegana:chives_5", "cucina_vegana:chives 4", "cucina_vegana:chives_1")
+    fn("cucina_vegana:flax_6", "cucina_vegana:flax_raw 4", "cucina_vegana:flax_1")
+    fn("cucina_vegana:kohlrabi_6", "cucina_vegana:kohlrabi 4", "cucina_vegana:kohlrabi_1")
+    fn("cucina_vegana:asparagus_6", "cucina_vegana:asparagus 4", "cucina_vegana:asparagus_1")
+    fn("cucina_vegana:lettuce_5", "cucina_vegana:lettuce 4", "cucina_vegana:lettuce_1")
+    fn("cucina_vegana:soy_8", "cucina_vegana:soy 6", "cucina_vegana:soy_1")
+    fn("cucina_vegana:peanut_7", "cucina_vegana:peanut 6", "cucina_vegana:peanut_1")
+    fn("cucina_vegana:rice_6", "cucina_vegana:rice 6", "cucina_vegana:rice_1")
+    fn("cucina_vegana:sunflower_5", "cucina_vegana:sunflower 4", "cucina_vegana:sunflower_1")
 end
 
 if minetest.get_modpath("bamboo") then
