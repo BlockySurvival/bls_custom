de60b9c0c28904acfb3e57b29991086c02b13553
flux <25628292+fluxionary@users.noreply.github.com>
2019-12-11 21:54:17 +0000

    add some 'missing' recipes to alloy smelter

--- terumet/alloy_smelter.lua
+++ terumet/alloy_smelter.lua
@@ -1,42 +1,17 @@
 if not minetest.global_exists("terumet") then return end
 
 terumet.register_alloy_recipe({
-    result="technic:carbon_steel_ingot 3",
-    input={"default:steel_ingot 3", "default:coal_lump"},
-    time=30,
-    flux=0,
-})
-terumet.register_alloy_recipe({
-    result="technic:carbon_steel_block 3",
-    input={"default:steelblock 3", "default:coalblock"},
-    time=100,
-    flux=0,
+    result="terumet:item_thermese",
+    input={"default:mese_crystal"},
+    time=6,
+    flux=4,
 })
 
 terumet.register_alloy_recipe({
-    result="technic:cast_iron_ingot 3",
-    input={"technic:carbon_steel_ingot 3", "default:coal_lump"},
-    time=60,
-    flux=0,
-})
-terumet.register_alloy_recipe({
-    result="technic:cast_iron_block 3",
-    input={"technic:carbon_steel_block 3", "default:coalblock"},
-    time=200,
-    flux=0,
-})
-
-terumet.register_alloy_recipe({
-    result="technic:stainless_steel_ingot 8",
-    input={"technic:carbon_steel_ingot 7", "technic:chromium_ingot"},
-    time=30,
-    flux=0,
-})
-terumet.register_alloy_recipe({
-    result="technic:stainless_steel_block 7",
-    input={"technic:carbon_steel_block 7", "technic:chromium_block"},
-    time=100,
-    flux=0,
+    result="terumet:block_thermese",
+    input={"default:mese"},
+    time=72,
+    flux=32,
 })
 
 if minetest.get_modpath("basic_materials") then
@@ -51,5 +26,55 @@ if minetest.get_modpath("basic_materials") then
     terumet.register_alloy_recipe{result="basic_materials:brass_ingot 3", flux=0, time=4.0, input={"default:copper_lump 2", "technic:zinc_lump"}}
     terumet.register_alloy_recipe{result="basic_materials:brass_ingot 3", flux=0, time=8.0, input={"default:copper_ingot 2", "technic:zinc_ingot"}}
     terumet.register_alloy_recipe{result="basic_materials:brass_block 3", flux=0, time=40.0, input={"default:copperblock 2", "technic:zinc_block"}}
-    terumet.register_alloy_recipe{result="basic_materials:brass_ingot 3", flux=0, time=2.0, input={"terumet:item_cryst_copper 2", zinc_crystal}}
+    terumet.register_alloy_recipe{result="basic_materials:brass_ingot 3", flux=0, time=2.0, input={"terumet:item_cryst_copper 2", "terumet:item_cryst_zinc"}}
+end
+
+if minetest.get_modpath("technic_worldgen") then
+    terumet.register_alloy_recipe({
+        result="technic:carbon_steel_ingot 3",
+        input={"default:steel_ingot 3", "default:coal_lump"},
+        time=30,
+        flux=0,
+    })
+    terumet.register_alloy_recipe({
+        result="technic:carbon_steel_block 3",
+        input={"default:steelblock 3", "default:coalblock"},
+        time=100,
+        flux=0,
+    })
+
+    terumet.register_alloy_recipe({
+        result="technic:cast_iron_ingot 3",
+        input={"technic:carbon_steel_ingot 3", "default:coal_lump"},
+        time=60,
+        flux=0,
+    })
+    terumet.register_alloy_recipe({
+        result="technic:cast_iron_block 3",
+        input={"technic:carbon_steel_block 3", "default:coalblock"},
+        time=200,
+        flux=0,
+    })
+
+    terumet.register_alloy_recipe({
+        result="technic:stainless_steel_ingot 8",
+        input={"technic:carbon_steel_ingot 7", "technic:chromium_ingot"},
+        time=30,
+        flux=0,
+    })
+    terumet.register_alloy_recipe({
+        result="technic:stainless_steel_block 7",
+        input={"technic:carbon_steel_block 7", "technic:chromium_block"},
+        time=100,
+        flux=0,
+    })
+end
+
+if minetest.get_modpath("xdecor") then
+    terumet.register_alloy_recipe({
+        result="terumet:block_ceramic",
+        input={"xdecor:hard_clay"},
+        time=48,
+        flux=16,
+    })
 end
