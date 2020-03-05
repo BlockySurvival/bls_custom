50569bad2750950d3bced98be9ea050a2b92fb09
flux <25628292+fluxionary@users.noreply.github.com>
2020-03-05 15:10:11 +0000

    add sulfur and nether ores to ore saw

--- terumet/ore_saw.lua
+++ terumet/ore_saw.lua
@@ -1,6 +1,15 @@
 if not minetest.global_exists("terumet") then return end
 
-terumet.options.ore_saw.VALID_ORES["technic:mineral_uranium"] = 1
-terumet.options.ore_saw.VALID_ORES["technic:mineral_lead"] = 1
-terumet.options.ore_saw.VALID_ORES["technic:mineral_zinc"] = 1
-terumet.options.ore_saw.VALID_ORES["technic:mineral_chromium"] = 1
+if minetest.get_modpath("nether") then
+    terumet.options.ore_saw.VALID_ORES["nether:sulfur_ore"] = 1
+    terumet.options.ore_saw.VALID_ORES["nether:titanium_ore"] = 1
+    terumet.options.ore_saw.VALID_ORES["nether:heart_ore"] = 1
+end
+
+if minetest.get_modpath("technic_worldgen") then
+    terumet.options.ore_saw.VALID_ORES["technic:mineral_uranium"] = 1
+    terumet.options.ore_saw.VALID_ORES["technic:mineral_lead"] = 1
+    terumet.options.ore_saw.VALID_ORES["technic:mineral_zinc"] = 1
+    terumet.options.ore_saw.VALID_ORES["technic:mineral_chromium"] = 1
+    terumet.options.ore_saw.VALID_ORES["technic:mineral_sulfur"] = 1
+end
