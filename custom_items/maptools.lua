8aacab121261bf4c1cffd147d440817ad1cf2722
flux <25628292+fluxionary@users.noreply.github.com>
2020-03-28 15:31:25 +0000

    add maptools:clean_glass

---
+++ custom_items/maptools.lua
@@ -0,0 +1,14 @@
+minetest.register_node(":maptools:clean_glass", {
+        description = S("Unbreakable Clean Glass"),
+        range = 12,
+        stack_max = 10000,
+        drawtype = "glasslike_framed_optional",
+        tiles = {"moreblocks_clean_glass.png", "moreblocks_clean_glass_detail.png"},
+        paramtype = "light",
+        sunlight_propagates = true,
+        is_ground_content = false,
+        drop = "",
+        groups = {unbreakable = 1, not_in_creative_inventory = maptools.creative},
+        sounds = default.node_sound_glass_defaults(),
+        on_drop = maptools.drop_msg
+})
