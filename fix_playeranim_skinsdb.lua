d9f4bfafccfc9ded6c4c709989a650e51dad01dc
luk3yx <luk3yx@users.noreply.github.com>
2020-07-11 21:37:22 +1200

    Fix player models in MT 5.3.

---
+++ fix_playeranim_skinsdb.lua
@@ -0,0 +1,9 @@
+if minetest.global_exists("player_api") then
+    local m = player_api.registered_models["skinsdb_3d_armor_character_5.b3d"]
+    if m and m.animations then
+        for _, v in pairs(m.animations) do
+            v.x = 0
+            v.y = 0
+        end
+    end
+end
