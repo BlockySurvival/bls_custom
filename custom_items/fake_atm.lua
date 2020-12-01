4de01a2a17a77c815197f9f8c81d7fa74d6ba9ee
luk3yx <luk3yx@users.noreply.github.com>
2020-12-01 21:18:21 +1300

    Replace lurkcoin:dummy_atm with lurkcoin:atm

--- custom_items/fake_atm.lua
+++ custom_items/fake_atm.lua
@@ -1,15 +1,9 @@
 -- dummy ATM for decoration
-if minetest.get_modpath("lurkcoin") and minetest.get_modpath("default") and minetest.get_modpath("dye") then
-    minetest.register_node(":lurkcoin:dummy_atm", {
-        description = "Dummy ATM",
-        groups = {cracky = 1},
-        tiles = {"lurkcoin_atm_side.png", "lurkcoin_atm_side.png",
-            "lurkcoin_atm_side.png", "lurkcoin_atm_side.png",
-            "lurkcoin_atm_side.png", "lurkcoin_atm_side.png^lurkcoin_atm_top.png"},
-        paramtype2 = "facedir",
-    })
+if minetest.get_modpath("lurkcoin") and minetest.get_modpath("default") and
+        minetest.get_modpath("dye") then
+    minetest.register_alias("lurkcoin:dummy_atm", "lurkcoin:atm")
     minetest.register_craft({
-        output = "lurkcoin:dummy_atm",
+        output = "lurkcoin:atm",
         type = "shapeless",
             recipe = {"default:steelblock", "dye:blue"},
     })
