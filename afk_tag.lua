4fd290c5bd9165dc905608d4172eac750c09e450
Oversword <bionc:oversword.co.uk>
2021-06-27 12:52:20 +0100

    Revert delay change

--- afk_tag.lua
+++ afk_tag.lua
@@ -3,7 +3,7 @@ if not minetest.get_modpath("more_monoids") then return end
 bls.afk = {}
 
 local AFK_CHECK_INTERVAL = 15  -- seconds
-local AFK_BOUND_PERIOD = 10 * 1 * 1000000 -- microseconds
+local AFK_BOUND_PERIOD = 10 * 60 * 1000000 -- microseconds
 
 local assertions_by_player = {}
 local last_action_by_player = {}
