a74e7712e8d31d9a09b53e4e4a71a310ecc1e920
Oversword <bionic@oversword.co.uk>
2021-07-18 12:05:26 +0100

    Add fake glowstone for andrew's quests

--- custom_items/fake_nodes.lua
+++ custom_items/fake_nodes.lua
@@ -6,3 +6,16 @@ minetest.register_node("bls:fake_stone", {
     groups = {cracky = 3, stone = 1, not_in_creative_inventory=1},
 })
 
+if minetest.get_modpath("nether") and minetest.global_exists("default") then
+    minetest.register_node("bls:fake_glowstone", {
+        description = "Fake Glowstone",
+        tiles = {"nether_glowstone.png"},
+        walkable = false,
+        light_source = 14,
+        paramtype = "light",
+        groups = {cracky = 3, oddly_breakable_by_hand = 3, not_in_creative_inventory=1},
+        sounds = default.node_sound_glass_defaults(),
+        on_blast = function (pos, intensity) end
+    })
+end
+
