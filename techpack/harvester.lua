808351df013d11c1f49743f43c2b47c1f5bb2b41
Oversword <bionic@oversword.co.uk>
2021-08-30 13:37:03 +0100

    Allow vanilla to be harvested by the harvester

--- techpack/harvester.lua
+++ techpack/harvester.lua
@@ -71,6 +71,7 @@ end
 if minetest.get_modpath("farming") then
     fn("farming:cabbage_6", "farming:cabbage 2", "farming:cabbage_1")
     fn("farming:mint_4", "farming:mint_leaf 4", "farming:mint_1")
+    fn("farming:vanilla_8", "farming:vanilla 4", "farming:vanilla_1")
 end
 
 if minetest.get_modpath("hollytree") then
