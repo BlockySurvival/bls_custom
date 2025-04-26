85b5b7f7566fcdc28924e2b5e254970d0ff84bf2
luk3yx <luk3yx@users.noreply.github.com>
2025-04-26 16:42:51 +1200

    Only use playeranim workaround if playeranim is installed

--- fix_playeranim_skinsdb.lua
+++ fix_playeranim_skinsdb.lua
@@ -1,4 +1,4 @@
-if minetest.global_exists("player_api") then
+if minetest.global_exists("player_api") and minetest.get_modpath("playeranim") then
     local m = player_api.registered_models["skinsdb_3d_armor_character_5.b3d"]
     if m and m.animations then
         for _, v in pairs(m.animations) do
